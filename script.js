96ecef894b5603adce23b53defdf7a91272d35f9
joshuatazrein <84872947+joshuatazrein@users.noreply.github.com>
2021-08-23 12:34:31 -0700

    tutorial and fixes

--- script.js
+++ script.js
@@ -228,7 +228,7 @@ function updateSizes() {
   for (list of [
     [$('#timerent')[0], 6], 
     [$('#searchbar')[0], 5],
-    [$('#username')[0], $('#username').val().length / 2 + 2],
+    [$('#username')[0], $('#username').text().length / 2 + 2],
     [$('#lists')[0], 7]
   ]) {
     // update entries
@@ -367,9 +367,9 @@ function download() {
 function reset() {
   yes = confirm("Are you sure you want to reset?")
   if (yes == true) {
-    data = {'flop': [{'title': 'inbox', 'text': '<span class="in h1" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">Welcome to RiverBank!</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">RiverBank is a tool for storing and scheduling tasks.</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">This is the Bank view, where you can bank unscheduled tasks and projects.</span><span class="in h1" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">tutorial</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">right-click to see all options (double-tap on mobile)</span><span class="in list" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">• Select and edit tasks<span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">1) press alt-enter to create a new task</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">2) press enter (or double-click) to save/edit</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">4) create subtasks with alt-shift-enter</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">5) delete tasks with delete</span></span><span class="in list" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">• Create a new project<span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">1) select "tutorial" by clicking on it ^^</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">2) press alt-enter to create a new task</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">3) type "# " to make it a heading</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">4) press enter to save your heading</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">5) select a heading or task and press "[" or "]" to fold/unfold</span></span><span class="in list" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">• Create a new list<span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">Lists are areas that store related tasks and projects</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">1) Right click on the "LISTS" area &lt;--</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">2) select "new"</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">3) enter the list name</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">4) click on a list\'s title to select it</span></span><span class="in list" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">• Schedule &amp; move tasks<span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">1) enter "d:t+1d" in the search bar (top left) for tomorrow</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">2) drag your task onto the new heading to schedule it --&gt;</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">3) alt-drag: add as subtask</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">4) cmd-drag: insert before</span></span><span class="in list" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">• format tasks<span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">1) [[links]] will search when you click on them</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">2) **bold**, *italic*, and **bold-italic**</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">3) weblink will automatically collapse</span></span><span class="in h2" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">extras</span><span class="in list" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">• search<span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">1) type into the searchbar (top left)</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">2) press enter to search</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">3) click on options to select them</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">4) press esc to clear search</span></span><span class="in list" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">• Search for dates<span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">1) enter "d:" to start date search</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">2) enter "t" for today</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">3) enter weekday to search weekdays (ex: "d:Mon"</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">4) enter modifiers after that to move forward/back: (+ or -)(number)(d/w/m/y)<span class="in list" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">•&nbsp;ex: Mon+1d, Mon-1d, Mon+3w, Mon+1m+2w</span><span class="in list" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">•&nbsp;Search will find the first weekday which matches and create a heading if it doesn\'t already exist</span></span></span><span class="in list" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">• set a timer<span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">1) enter minutes into "timer" (top left)</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">2) press enter to start, space to stop, and esc to clear</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="false" style="">3) 25, 15, 5: set minutes</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">4) +2, -2: add/subtract 2 minutes</span></span><span class="in list" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">• customize settings: click on options (top left)<span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">1) change date between dd.mm.yyyy, mm/dd/yyyy, and yyyy-mm-dd</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">2) change weekdays between long (Mon) and short (M)</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">3) change color themes</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">4) hide/show buttons</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">5) hide/show help</span></span>'}],'pop': '<span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">This is the River view, where you can drag tasks to specific dates to schedule them.</span><span class="in list" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">• Complete tasks<span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">1) select the task</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">2) press space to complete in place</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">3) OR press shift-space to archive (moves to "completed" for today</span></span><span class="in list" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">•&nbsp;create events<span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">1) create a new task</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">2) enter "@ " to make an event and save</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">3) drag tasks onto the event to schedule them for that block of time</span></span><span class="in list" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">• create deadlines<span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">1) create a new task</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">2) enter "&gt;" and a date or weekday</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">3) enter the task name and save</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">4) a red heading is displayed under the due date (see below)</span><span class="in" ondragstart="dragTask(event)" ondragover="draggingOver(event)" ondrop="dropTask(event)" draggable="true" style="">5) click on due date or deadline to navigate between them</span></span>', 'hidebuts':'false', 'style':'default.css', 'dateSplit':'mm/dd/yyyy', 'weekdays':'Mon', 'help':'show', 'loadedlist':'0'}
-    dataString = JSON.stringify(data)
-    localStorage.setItem('data', dataString)
+    console.log('resetting')
+    data = JSON.parse(JSON.stringify(resetstring))
+    localStorage.setItem('data', JSON.stringify(data))
     uploadData()
     reloadpage()
   }
@@ -638,6 +638,7 @@ function stringToDate(string, weekday) {
 
 function dateToHeading(date) {
   if (date === undefined) return
+  if (dateToString(date).includes('NaN')) return
   // find the matching date, or create if not
   const newtask = createBlankTask()
   newtask.addClass('h1')
@@ -1037,16 +1038,6 @@ function saveTask() {
     selected.parent().attr('draggable', 'true')
   }
   save()
-}
-
-function helpfold(el) {
-  for (child of el.children().toArray().slice(1)) {
-    if ($(child).is(':hidden')) {
-      $(child).show()
-    } else {
-      $(child).hide()
-    }
-  }
 }
 
 function select(el, scroll) {
@@ -1464,12 +1455,6 @@ function getHeadingChildren(el) {
   return children.toArray().slice(start).map((x) => {return $(x)})
 }
 
-function collapseAll(folded) {
-  $('span').toArray().forEach((x) => {
-    if ($(x).attr('folded') == folded) {togglefold($(x));}
-  })
-}
-
 // toggle fold of a heading
 function togglefold(e, saving) {
   children = e.parent().children().toArray()
@@ -1561,6 +1546,9 @@ function context(e) {
   } else {
     select($(e.target), false)
   }
+  if ($(e.target).hasClass('buffer')) {
+    $('.buffer').remove();
+  };
   $('#context-menu').show()
   options = {
     '#context-newlist': [['TEXTAREA', 'DIV'], ['selected', 'unselected', 
@@ -1587,8 +1575,6 @@ function context(e) {
     '#context-toggleSubtasks' : [['SPAN'], ['in']],
     '#context-archiveComplete' : [['SPAN', 'P'], ['in']],
     '#context-clearEmptyHeadlines' : [['P'], ['in']],
-    '#context-collapseAll' : [['P', 'SPAN'], ['in']],
-    '#context-expandAll' : [['P', 'SPAN'], ['in']],
     '#context-toggleButs' : [['BUTTON'], ['opts']],
     '#context-styleDefault' : [['BUTTON'], ['opts']],
     '#context-styleJason' : [['BUTTON'], ['opts']],
@@ -2126,7 +2112,7 @@ function reloadpage() {
 }
 
 function loadpage(setload) {
-  $('#username').val(document.cookie.split(';')[0].split('=')[1].split('_')[0])
+  $('#username').text(document.cookie.split(';')[0].split('=')[1].split('_')[0])
   if (setload != false) {
     // prevents endless loading loop
     $(document).on('keydown', keycomms)
@@ -2169,9 +2155,9 @@ function loadpage(setload) {
     $(':root').css('--butheight', '-10px')
   }
   $('.taskselect').removeClass('taskselect')
-  $('#help').children().toArray().forEach((x) => {helpfold($(x))})
   updateSizes()
   clearEmptyDates()
 }
 
 loadpage()
+console.log(JSON.stringify(data))
