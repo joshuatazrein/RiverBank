a4323a109a4e0bd5768cac645101793228217480
Joshua Tazman Reinier <84872947+joshuatazrein@users.noreply.github.com>
2021-08-24 12:57:21 -0700

    dateToHeading

--- script.js
+++ script.js
@@ -670,7 +670,7 @@ function dateToHeading(date) {
     $(x).hasClass('dateheading')) return true
   })
   let heading1 = headingslist.find((x) => {
-    if (stringToDate(stripChildren($(x))) == stringToDate(newtask.text())) return true
+    return stringToDate(stripChildren($(x))).getTime() == stringToDate(newtask.text()).getTime()
   })
   if (heading1 == undefined) {
     // insert elt at beginning
