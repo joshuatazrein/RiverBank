0893c416393384a765b4b6334313c6273af95bc7
Joshua Tazman Reinier <84872947+joshuatazrein@users.noreply.github.com>
2021-08-30 02:16:40 -0700

    fixes morning

--- script.js
+++ script.js
@@ -271,6 +271,26 @@ function toggleFoldList() {
 }
 
 function updateSizes() {
+  for (list of [
+    [$('#timerent')[0], 6],
+    [$('#searchbar')[0], 5],
+    [$('#username')[0], $('#username').text().length / 2 + 2],
+    [$('#lists')[0], 7]
+  ].concat(
+    $('#loads').children().toArray().map((x) => {
+      let textlength = Math.max($(x).val().split(' ').map((x) => {
+        return x.length
+      }))
+      return [$(x)[0], textlength / 2 + 2.5]
+    })
+  )) {
+    // update entries
+    let fontsize = 24
+    while ($(list[0]).width() / (fontsize / 2) < list[1]) {
+      fontsize -= 1
+    }
+    $(list).css('font-size', fontsize + 'px')
+  }
   // fix context menu for mobile
   if (window.innerWidth < 600) {
     $('.dropdown-item').toArray().forEach((x) => {
@@ -354,23 +374,26 @@ function save(undo) {
     }
   }
   // save data
-  data.pop = $('#pop').html()
+  let newdata = JSON.parse(JSON.stringify(data)) // copies data
+  newdata.pop = $('#pop').html()
   if (loadedlist != undefined) {
     try {
-      data.flop[loadedlist].text = $('#flop').html()
+      newdata.flop[loadedlist].text = $('#flop').html()
       try {
-        data.flop[loadedlist].title = $('#loads').children()[loadedlist].value
+        newdata.flop[loadedlist].title = $('#loads').children()[loadedlist].value
       } catch (TypeError) {
-        data.flop[loadedlist].title = ''
+        newdata.flop[loadedlist].title = ''
       }
-      data.loadedlist = loadedlist
+      newdata.loadedlist = loadedlist
     } catch (TypeError) {
-      data.loadedlist = 0
+      newdata.loadedlist = 0
       loadedlist = 0
       loadList()
     }
   }
-  dataString = JSON.stringify(data)
+  dataString = JSON.stringify(newdata)
+  if (dataString == JSON.stringify(data)) return // stops redundancies
+  data = JSON.parse(dataString)
   localStorage.setItem('data', dataString)
   // backup data to the server after setting localstorage data
   uploadData()
@@ -1205,7 +1228,6 @@ function saveTask() {
   // analyze format of task and create new <span> elt for it
   const savetask = selected.prev() // looks at item before it
   selected.next().remove() // removes appended children
-  console.log([selected.val()])
   if (['', '• ', '- ', '# ', '## ', '### ', '@', '@ '
     ].includes(selected.val()) || 
     /^•\s$/.test(selected.val())) {
@@ -1320,6 +1342,10 @@ function saveTask() {
     }
   }
   newstr += htmlstr.slice(start)
+  console.log(newstr);
+  newstr = newstr.replace(
+    /\n/g, '<br>').replace(/\s/g, ' ')
+  console.log(newstr, getChildren(el));
   newstr += getChildren(el)
   if (selected.val().charAt(0) == '@') {
     // process event signs
@@ -1343,8 +1369,7 @@ function saveTask() {
   newstr = newstr.replace(
     /\_(.*)\_/, "$1").replace(
     /\*(.*)\*/, "$1").replace(
-    /\_*(.*)\*_/, "$1").replace(
-    '\n', '<br>').replace(/\s/, ' ')
+    /\_*(.*)\*_/, "$1")
   savetask.html(newstr)
   try {
     // fixing weird glitch
@@ -1488,6 +1513,8 @@ function setText(el) {
 function stripChildren(el, mode) {
   // retrieve text from only the parent span and any of its formatting
   const testelt = el.clone()
+  testelt.html(testelt.html().replace(/<br>/g, '\n'))
+  console.log(testelt.html(),' stripchildren');
   for (child of testelt.children()) {
     // strip the subtasks
     if (isSubtask($(child)) == true) {
@@ -1495,6 +1522,9 @@ function stripChildren(el, mode) {
     } else if ($(child).hasClass('weblink') == true) {
       $(child).html($($(child).children()[0]).attr('href'))
     }
+  }
+  while (testelt.text().charAt(testelt.text().length - 1) == '\n') {
+    testelt.text(testelt.text().slice(0, testelt.text().length - 1))
   }
   if (mode == undefined) {
     return testelt.text()
@@ -1509,7 +1539,7 @@ function isSubtask(el) {
       'link', 'italic', 'bold', 'bold-italic', 'deadline', 'weblink', 'timing',
       'mobhandle'
     ]) {
-    if (el.hasClass(lineinner) == true) {
+    if (el.hasClass(lineinner) == true || el.hasClass('in') == false) {
       return false
       break
     }
@@ -1531,6 +1561,7 @@ function getChildren(el) {
       break
     }
   }
+  while (newstr.slice(0, 4) == '<br>') newstr = newstr.slice(4)
   return newstr
 }
 
@@ -1538,7 +1569,7 @@ function updateHeight() {
   // $('#texttest').font(selected.css('font'))
   $('#texttest').text(selected.val() + ' x')
   $('#texttest').css('width', selected.width() + 'px')
-  selected.css('height', $('#texttest').height() + 'px')
+  selected.css('height', $('#texttest').height() + 5 + 'px')
 }
 
 function editTask() {
@@ -1569,7 +1600,7 @@ function editTask() {
       '<span class="bold">*$1*</span>').replace(
       /<span class="bold-italic">(.*)<\/span>/,
       '<span class="bold-italic">_*$1*_</span>'))
-    selected.val(stripChildren(el).replace('<br>', '\n')) 
+    selected.val(stripChildren(el)) 
     // add back hashtags
     if (el.hasClass('h1')) {
       selected.val('# ' + selected.val())
@@ -1701,6 +1732,7 @@ function toggleComplete() {
       const date = stringToDate(text[text.length - 1].slice(1), false, true)
       const heading = dateToHeading(date)
       const newtask = selected.clone()
+      newtask.removeClass('complete')
       if (
         !getHeadingChildren($(heading)).map((x) => {
           return $(x).text()
@@ -1814,6 +1846,7 @@ function dropTask(evt, obj) {
     children = getHeadingChildren(selected)
   }
   if ($(el).attr('folded') == 'true') {
+    // unfold
     togglefold($(el))
     if (getHeadingChildren($(el)).length == 0) {
       $(el).after(selected)
@@ -1827,17 +1860,6 @@ function dropTask(evt, obj) {
       $(el).parent().prepend(selected)
     } else {
       $(el).before(selected)
-    }
-  } else if (el.tagName == 'P' && $(el).hasClass('in')) {
-    // drop task at beginning
-    if (evt.altKey == true) {
-      if (evt.metaKey == true) {
-        $(el).prepend(selected)
-      } else {
-        $(el).append(selected)
-      }
-    } else {
-      $(el).append(selected)
     }
   } else if (el.tagName == 'SPAN' &&
     $(el).hasClass('in')) {
@@ -2019,7 +2041,8 @@ function setStyle(style) {
   data.style = style
   save()
   uploadData(false)
-  location.reload()
+  load()
+  reloadpage()
 }
 
 function context(e, mobile) {
@@ -2263,35 +2286,39 @@ function selectRandom() {
 
 function setTask(type) {
   if (!selected || selected.hasClass('dateheading')) return
-  const list = selected.text().split(' ').filter((x) => {return x != ''})
+  const list = stripChildren(selected).split(' ').filter((x) => {return x != ''})
   console.log(list);
   if (type == 'deadline') {
     if (!/\>/.test(list[list.length - 1])) {
-      selected.text(selected.text() + ' >')
+      selected.html(stripChildren(selected) + ' >' + 
+        getChildren(selected))
     } else {
-      selected.text(list.slice(0, list.length - 1).join(' ') + ' >')
+      selected.html(
+        list.slice(0, list.length - 1).join(' ') + ' >' + 
+        getChildren(selected))
     }
     editTask()
   } else if (type == 'repeat') {
     if (!/~/.test(list[list.length - 1])) {
-      selected.text(selected.text() + ' ~')
+      selected.html(stripChildren(selected) + ' ~' + getChildren(selected))
     } else {
-      selected.text(list.slice(0, list.length - 1).join(' ') + ' ~')
+      selected.html(
+        list.slice(0, list.length - 1).join(' ') + ' ~' + getChildren(selected))
     }
     editTask()
   } else {
     selected.attr('class', 'in ' + type)
     if (['-', '•'].includes(selected.text().charAt(0))) {
       // remove bullet or list
-      selected.text(selected.text().slice(1))
+      selected.html(selected.html().slice(1))
       if (selected.text().charAt(0) == ' ') {
-        selected.text(selected.text().slice(1))
+        selected.html(selected.html().slice(1))
       }
     }
     if (type == 'list') {
-      selected.text('• ' + selected.text())
+      selected.html('• ' + selected.html())
     } else if (type == 'note') {
-      selected.text('- ' + selected.text())
+      selected.html('- ' + selected.html())
     }
     save()
   }
@@ -2392,7 +2419,7 @@ function clicked(ev) {
     let title = $(ev.target).attr('title')
     if (!title.includes('https://www.')) title = 'https://www.' + title
     else if (!title.includes('https://')) title = 'https://' + title
-    window.location = title
+    window.open(title, '_blank')
   } else if ($(ev.target).hasClass('timing')) {
     // timing generate for thing
     dragTime($(ev.target))
@@ -2724,7 +2751,6 @@ function keycomms(evt) {
     if (evt.key == 'ArrowUp' && evt.shiftKey) {
       evt.preventDefault()
       while (taskAbove() && !isHeading(taskAbove())) {
-        console.log(taskBelow());
         if (taskAbove()[0] == selected[0]) break
         select(taskAbove())
       }
@@ -2901,6 +2927,9 @@ function loadpage(setload) {
   // go to today
   updatedeadlines()
   $('#pop').scrollTop(0)
+  $('#pop').scrollTop(
+    $(dateToHeading(stringToDate('t'))).prev().offset().top - 
+    $('#pop').offset().top)
   select(dateToHeading(stringToDate('t')))
   loading = false
   $(document).scrollTop(0)
