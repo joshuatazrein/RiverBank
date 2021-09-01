04df079d8c17aab24971eaec863fd9ea679379a6
Joshua Tazman Reinier <84872947+joshuatazrein@users.noreply.github.com>
2021-09-01 10:08:54 -0700

    Revert "uploadphp"
    This reverts commit 7dbb407172e309d4ca8a63d5b2ea732f796621f6.

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, " . 
-"Content-Type, Accept, Cache-Control");
+// header("Access-Control-Allow-Headers: Origin, X-Requested-With, " . 
+// "Content-Type, Accept, Cache-Control");
 $cookie = $_COOKIE['username'];
 $file = fopen('users/' . $cookie . '.json', 'w');
 fwrite($file, $_POST['data']);
