403735ed83444bae1186bd60bc169b4c5a37d4b6
Joshua Tazman Reinier <84872947+joshuatazrein@users.noreply.github.com>
2021-09-03 14:27:09 -0700

    getuser

--- getuser.php
+++ getuser.php
@@ -6,10 +6,9 @@ $conn = new mysqli('server204.web-hosting.com',
 if ($conn->connect_error) {
   die('Connection failed: ' . $conn->connect_error);
 }
-mysqli_real_query($conn, 
-'SELECT fname
-FROM users
-WHERE user="' . $username . '" AND ' . 'pw="' . $password . '"');
-$result = mysqli_use_result($conn);
-echo json_encode(mysqli_fetch_all($result));
+$sql = 'SELECT fname WHERE user="' . $username . 
+  '" AND pw="' . $password . '"';
+$result = $conn->query($sql);
+$row = $result->fetch_array(MYSQLI_NUM);
+print($row[0][0]);
 ?>
