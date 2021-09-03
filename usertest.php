58320f0774981764edc63a869e7023ce75b875e7
Joshua Tazman Reinier <84872947+joshuatazrein@users.noreply.github.com>
2021-09-03 06:34:06 -0700

    database

--- usertest.php
+++ usertest.php
@@ -1,8 +1,9 @@
 <?php
 $servername = 'server204.web-hosting.com';
+$database = 'joshgncd_riverbank';
 $username = 'joshcnd_joshua';
 $password = 'R1v3rB4nk!';
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
   die('Connection failed: ' . $conn->connect_error);
 }
