ddfa5b999fc936e463da14b9c55e13a269b03ab5
Joshua Tazman Reinier <84872947+joshuatazrein@users.noreply.github.com>
2021-09-03 08:15:21 -0700

    loadlist, usertest

--- usertest.php
+++ usertest.php
@@ -1,9 +1,9 @@
 <?php
 $servername = 'server204.web-hosting.com';
 $database = 'joshgncd_riverbank';
-$username = 'joshcnd_joshua';
+$username = 'joshgcnd_joshua';
 $password = 'R1v3rB4nk!';
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database, 3306);
 if ($conn->connect_error) {
   die('Connection failed: ' . $conn->connect_error);
 }
