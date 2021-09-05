57f82d9f4cc0cc6717864b1f67db91ab7f7661c1
Joshua Tazman Reinier <84872947+joshuatazrein@users.noreply.github.com>
2021-09-05 03:12:23 -0700

    loads

--- script.js
+++ script.js
@@ -3300,16 +3300,9 @@ function loadpage(setload, oldselect) {
     else if (oldselect[0])
       select(oldselect[0])
   } else {
-    select()
-    select($(dateToHeading(stringToDate('0d'))), false)
-    $('#pop').scrollTop(0)
-    const scrolllocation = selected.offset().top - 
-      $('#pop').offset().top - 30
     setTimeout(function () {
-      $('#pop').animate({
-        scrollTop: scrolllocation
-      }, 500)
-    }, 300)
+      $('#todayBut').click()
+    }, 750)
   }
 }
 
