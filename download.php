a0a09286a06e72c276ee5b8de6be8dfe5bd381b1
Joshua Tazman Reinier <84872947+joshuatazrein@users.noreply.github.com>
2021-09-28 02:49:05 -0400

    Update download.php

--- download.php
+++ download.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 // $user = $_COOKIE['user'];
 // $pw = $_COOKIE['pw'];
 // $conn = new mysqli('server204.web-hosting.com', 
@@ -18,6 +19,26 @@
 //   // echo "FAIL";
 // }
 // $conn->close();
+=======
+$user = $_COOKIE['user'];
+$pw = $_COOKIE['pw'];
+$conn = new mysqli('server204.web-hosting.com', 
+  'joshgncd_joshua', 'hn%X=FbWIU]J', 'joshgncd_riverbank', 3306);
+if ($conn->connect_error) {
+  die('FAIL');
+}
+$sql = 'SELECT dt FROM users WHERE user="' . 
+  $username . '" AND pw="' . $password . '"';
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+  // output data of each row
+  while ($row = $result->fetch_assoc()) {
+    // echo $row["dt"];
+  }
+} else {
+  // echo "FAIL";
+}
+>>>>>>> parent of 33f3d69 (orig download)
 // original
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
 $cookie = $_COOKIE['fname'];
