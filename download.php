aef247cc108475b8dd4f76b5c5393f1467466aa6
Joshua Tazman Reinier <84872947+joshuatazrein@users.noreply.github.com>
2021-09-28 03:05:53 -0400

    fixed

--- download.php
+++ download.php
@@ -1,7 +1,5 @@
 <?php
-// original
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
 $cookie = $_COOKIE['fname'];
 echo file_get_contents('users/' . $cookie . '.json');
-$conn->close();
 ?>
