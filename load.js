7febb184888588510dc2e5593250a12f092422b5
Joshua Tazman Reinier <84872947+joshuatazrein@users.noreply.github.com>
2021-10-04 16:53:05 -0400

    abort get

--- load.js
+++ load.js
@@ -115,9 +115,11 @@ function load() {
   getting = setInterval(function () {console.log('getting...')}, 1000)
   setTimeout(function() {
     display('get timed out, downloading from local')
-    console.log(localStorage.getItem('data'))
-    data = JSON.parse(JSON.parse(localStorage.getItem('data')))
-    console.log(data);
+    data = JSON.parse(localStorage.getItem('data'))
+    while (!data.flop) {
+      data = JSON.parse(data)
+    }
+    display('new data:', data)
     clearInterval(getting)
     initialize()
   }, 7000)
